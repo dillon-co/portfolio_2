a61812f6aa29afd91acd470471bd4dc515480391
grandmasterspoc <dilloncortez@gmail.com>
2016-02-04 20:40:21 -0700

    Added line, thanks rocco.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ProgrammingPortfolio2
   class Application < Rails::Application
+    config.assets.precompile =  ['*.js', '*.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
